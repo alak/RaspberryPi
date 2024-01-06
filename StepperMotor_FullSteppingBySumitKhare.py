4651ce999b81e47a290874edad48596aaf99206d
Cathaly Kevin <alaknorth@gmail.com>
2024-01-06 14:10:03 +0100

    test other lib

--- StepperMotor_FullSteppingBySumitKhare.py
+++ StepperMotor_FullSteppingBySumitKhare.py
@@ -30,17 +30,28 @@
 #    print("release")
 #    led_line.release()
 
-import gpiod
-import time
-LED_PIN = 21
-chip = gpiod.Chip('gpiochip4')
-led_line = chip.get_line(LED_PIN)
-led_line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
-try:
-   while True:
-       led_line.set_value(1)
-       time.sleep(1)
-       led_line.set_value(0)
-       time.sleep(1)
-finally:
-   led_line.release()
+# import gpiod
+# import time
+# LED_PIN = 21
+# chip = gpiod.Chip('gpiochip4')
+# led_line = chip.get_line(LED_PIN)
+# led_line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
+# try:
+#    while True:
+#        led_line.set_value(1)
+#        time.sleep(1)
+#        led_line.set_value(0)
+#        time.sleep(1)
+# finally:
+#    led_line.release()
+
+from gpiozero import LED
+from time import sleep
+
+led = LED(21)
+
+while True:
+    led.on()
+    sleep(1)
+    led.off()
+    sleep(1)

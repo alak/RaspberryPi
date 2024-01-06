95b2f91fa3eb0dd802d922c358eda3a07b498141
Cathaly Kevin <alaknorth@gmail.com>
2024-01-06 13:50:23 +0100

    led

--- StepperMotor_FullSteppingBySumitKhare.py
+++ StepperMotor_FullSteppingBySumitKhare.py
@@ -1,31 +1,46 @@
+# import gpiod
+# import time
+# ENA_PIN = 4
+# IN1_PIN = 17
+# IN2_PIN = 27
+# chip = gpiod.Chip('gpiochip4')
+# ena_line = chip.get_line(ENA_PIN)
+# ena_line.request(consumer="ENA_MOTOR", type=gpiod.LINE_REQ_DIR_OUT, default_vals=[0])
+
+# in1_line = chip.get_line(IN1_PIN)
+# in1_line.request(consumer="IN1_MOTOR", type=gpiod.LINE_REQ_DIR_OUT, default_vals=[0])
+
+# in2_line = chip.get_line(IN2_PIN)
+# in2_line.request(consumer="IN2_MOTOR", type=gpiod.LINE_REQ_DIR_OUT, default_vals=[0])
+# try:
+#    while True:
+#        print("start")
+#        ena_line.set_value(1)
+#        in1_line.set_value(1)
+#        in2_line.set_value(0)
+#        print("sleep")
+#        time.sleep(20)
+#        print("endsleep")
+#        ena_line.set_value(0)
+#        in1_line.set_value(0)
+#        print("stop")
+#        time.sleep(1)
+       
+# finally:
+#    print("release")
+#    led_line.release()
+
 import gpiod
 import time
-ENA_PIN = 4
-IN1_PIN = 17
-IN2_PIN = 27
+LED_PIN = 21
 chip = gpiod.Chip('gpiochip4')
-ena_line = chip.get_line(ENA_PIN)
-ena_line.request(consumer="ENA_MOTOR", type=gpiod.LINE_REQ_DIR_OUT, default_vals=[0])
-
-in1_line = chip.get_line(IN1_PIN)
-in1_line.request(consumer="IN1_MOTOR", type=gpiod.LINE_REQ_DIR_OUT, default_vals=[0])
-
-in2_line = chip.get_line(IN2_PIN)
-in2_line.request(consumer="IN2_MOTOR", type=gpiod.LINE_REQ_DIR_OUT, default_vals=[0])
+led_line = chip.get_line(LED_PIN)
+led_line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
 try:
    while True:
-       print("start")
-       ena_line.set_value(1)
-       in1_line.set_value(1)
-       in2_line.set_value(0)
-       print("sleep")
-       time.sleep(20)
-       print("endsleep")
-       ena_line.set_value(0)
-       in1_line.set_value(0)
-       print("stop")
+       led_line.set_value(1)
        time.sleep(1)
-       
+       led_line.set_value(0)
+       time.sleep(1)
 finally:
-   print("release")
    led_line.release()
